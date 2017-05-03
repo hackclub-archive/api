036b56aa30e541279c755a24da3cf2e05a29cd87
Max Wofford <max@maxwofford.com>
2017-05-03 15:08:40 -0700

    Add active admin command

--- app/models/hackbot/interactions/active.rb
+++ app/models/hackbot/interactions/active.rb
@@ -1,14 +1,14 @@
 module Hackbot
   module Interactions
-    class Dormant < AdminCommand
+    class Active < AdminCommand
       include Concerns::LeaderAssociable
 
-      TRIGGER = /set-club dormant ?(?<subject_identifier>.+)/
+      TRIGGER = /set-club active ?(?<subject_identifier>.+)/
 
-      USAGE = 'set-club <dormant|active> <club_streak_key>'.freeze
-      DESCRIPTION = 'pause check-ins for a club during summer break'.freeze
+      USAGE = 'set-club active <club_streak_key>'.freeze
+      DESCRIPTION = 'turn on weekly check-ins'.freeze
 
-      DORMANT_STAGE_KEY = '5014'.freeze
+      ACTIVE_STAGE_KEY = '5003'.freeze
 
       def start
         streak_key = captured[:subject_identifier]
@@ -16,8 +16,8 @@ module Hackbot
         @club = Club.find_by(streak_key: streak_key)
         not_found && return unless @club
 
-        @club.stage_key = DORMANT_STAGE_KEY
-        StreakClient::Box.update(streak_key, 'stageKey' => DORMANT_STAGE_KEY)
+        @club.stage_key = ACTIVE_STAGE_KEY
+        StreakClient::Box.update(streak_key, 'stageKey' => ACTIVE_STAGE_KEY)
         stage_changed if @club.save
       end
 
