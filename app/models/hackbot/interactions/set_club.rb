59e596f82124439d7f5bc06768dcb44398b58b78
Max Wofford <max@maxwofford.com>
2017-05-05 00:00:40 -0700

    Fix rubocop

--- app/models/hackbot/interactions/set_club.rb
+++ app/models/hackbot/interactions/set_club.rb
@@ -5,21 +5,26 @@ module Hackbot
 
       TRIGGER = /set-club *(?<args>.*)/
 
-      USAGE = 'set-club <dormant|active> <club_streak_key>'.freeze
+      USAGE = 'set-club <dormant|active> <club_streak_key (admin only)>'.freeze
       DESCRIPTION = 'pause check-ins for your club during summer break'.freeze
 
       STAGES = {
         'active' => '5003',
         'dormant' => '5014'
-      }
+      }.freeze
 
       def should_handle?
-        super &&
-          event[:user] == data['slack_id'] if state == 'wait_for_club_select'
+        if state == 'wait_for_club_select'
+          super && event[:user] == data['slack_id']
+        else
+          super
+        end
       end
 
+      # rubocop:disable Metrics/AbcSize
       def start
-        arr = captured[:args].split ' '
+        arr = captured[:args].split(' ')
+
         data['stage_name'] = arr.first.downcase
         data['stage_key'] = STAGES[data['stage_name']]
         data['streak_key'] = arr.second
@@ -27,22 +32,19 @@ module Hackbot
 
         invalid_stage && return unless data['stage_key']
 
-        if data['streak_key']
-          admin_change
-        else
-          leader_change
-        end
+        data['streak_key'] ? admin_run : leader_run
       end
+      # rubocop:enable Metrics/AbcSize
 
       private
 
-      def admin_change
+      def admin_run
         data['club'] = Club.find_by(streak_key: data['streak_key'])
         not_found if data['club'].nil?
         set_single_club
       end
 
-      def leader_change
+      def leader_run
         case leader.clubs.count
         when 0
           msg_channel copy('no_clubs_found')
@@ -77,25 +79,31 @@ module Hackbot
         set_single_club
       end
 
+      # rubocop:disable Metrics/AbcSize
       def set_single_club
         if data['club'].stage_key == data['stage_key']
           already_in_stage
         else
           data['club'].stage_key = data['stage_key']
+          data['club'].save
 
-          stage_changed if data['club'].save
+          stage_changed
         end
         :finish
       end
+      # rubocop:enable Metrics/AbcSize
 
       def stage_changed
-        reaction = copy("reaction.#{data['stage_name']}") || copy('reaction.else')
+        reaction = copy("reaction.#{data['stage_name']}") ||
+                   copy('reaction.else')
         msg_channel copy('stage_changed', name: data['club'].name,
-                         stage_name: data['stage_name'], reaction: reaction)
+                                          stage_name: data['stage_name'],
+                                          reaction: reaction)
       end
 
       def already_in_stage
-        msg_channel copy('already_in_stage', stage_name: data['club'].stage_name)
+        msg_channel copy('already_in_stage',
+                         stage_name: data['club'].stage_name)
       end
 
       def not_found
