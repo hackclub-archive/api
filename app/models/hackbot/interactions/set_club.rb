eb24abc5b67ee3e182c410922375fe5361c8a54e
Max Wofford <max@maxwofford.com>
2017-05-08 10:19:10 -0700

    Add activation date prompt to SetClub interaction

--- app/models/hackbot/interactions/set_club.rb
+++ app/models/hackbot/interactions/set_club.rb
@@ -14,11 +14,7 @@ module Hackbot
       }.freeze
 
       def should_handle?
-        if state == 'wait_for_club_select'
-          super && event[:user] == data['slack_id']
-        else
-          super
-        end
+        super && event[:user] == data['slack_id'] unless state == 'start'
       end
 
       # rubocop:disable Metrics/MethodLength
@@ -43,6 +39,34 @@ module Hackbot
       # rubocop:enable Metrics/MethodLength
       # rubocop:enable Metrics/AbcSize
 
+      def wait_for_club_select
+        return :wait_for_club_select unless action
+
+        data['club'] = Club.find action[:value]
+
+        send_action_result copy('many_clubs.action_result',
+                                club_name: data['club'].name)
+
+        set_single_club
+      end
+
+      # rubocop:disable Metrics/AbcSize
+      def wait_for_activation_date
+        return :wait_for_club_select unless msg
+
+        data['activation_date'] = Chronic.parse(msg, bias: :future)
+        human_date = data['activation_date']
+                     .to_date
+                     .to_formatted_s :long_ordinal
+        msg_channel copy('activation_date.confirmation',
+                         activation_date: human_date)
+
+        data['club'].update(activation_date: data['activation_date'])
+
+        :finish
+      end
+      # rubocop:enable Metrics/AbcSize
+
       private
 
       def set_one_in_many_clubs
@@ -58,17 +82,6 @@ module Hackbot
         :wait_for_club_select
       end
 
-      def wait_for_club_select
-        return :wait_for_club_select unless action
-
-        data['club'] = Club.find action[:value]
-
-        send_action_result copy('many_clubs.action_result',
-                                club_name: data['club'].name)
-
-        set_single_club
-      end
-
       # rubocop:disable Metrics/AbcSize
       def set_single_club
         if data['club'].stage_key == data['stage_key']
@@ -79,17 +92,24 @@ module Hackbot
 
           stage_changed
         end
-        :finish
       end
       # rubocop:enable Metrics/AbcSize
 
+      # rubocop:disable Metrics/AbcSize
       def stage_changed
         reaction = copy("reaction.#{data['stage_name']}") ||
                    copy('reaction.else')
         msg_channel copy('stage_changed', name: data['club'].name,
                                           stage_name: data['stage_name'],
                                           reaction: reaction)
+
+        return unless data['stage_name'] == 'dormant'
+
+        msg_channel copy('activation_date.prompt')
+
+        :wait_for_activation_date
       end
+      # rubocop:enable Metrics/AbcSize
 
       def already_in_stage
         msg_channel copy('already_in_stage',
